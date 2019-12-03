4e7937b137d28842f7650a903c7c83df823cf25c
climber1982 <403783154@qq.com>
2019-12-03 16:51:59 +0800

    inti

---
+++ springObj/src/main/java/com/lovo/spring/bean/UserBean.java
@@ -0,0 +1,22 @@
+package com.lovo.spring.bean;
+
+public class UserBean {
+    private  String UserName;
+    private  int  age;
+
+    public String getUserName() {
+        return UserName;
+    }
+
+    public void setUserName(String userName) {
+        UserName = userName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}

6e9b42df13152c740edbebe062eae3729ffd1a93
climber1982 <403783154@qq.com>
2019-12-16 17:03:09 +0800

    inti

--- springmvc/src/main/java/com/lovo/mvc/controller/UserController.java
+++ springmvc/src/main/java/com/lovo/mvc/controller/UserController.java
@@ -4,12 +4,16 @@ import com.lovo.mvc.entity.UserEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
 @Controller //被spring管理
 public class UserController {
 
    @RequestMapping("savaUser.lovo")
-    public String savaUser(UserEntity userEntity ){
+    public String savaUser(UserEntity userEntity, String userName, String userAge, ServletRequest request, ServletResponse response){
        System.out.println(userEntity.getUserName()+"/"+userEntity.getUserAge());
+
         return "index.jsp";
     }
    @RequestMapping("findUser.lovo")
